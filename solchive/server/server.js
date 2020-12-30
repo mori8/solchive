4d5baef8b52a56da214e995b4425ab290df820d2
songfox00 <songfox00@gmail.com>
2020-12-30 13:30:27 +0900

    server

---
+++ solchive/server/server.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const data = fs.readFileSync('../database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+})
+
+connection.connect()
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
