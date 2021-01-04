288931ffed68548a7aa89a5e4976506d19a6846d
mori8 <kaithape@gmail.com>
2021-01-04 22:30:12 +0900

    Header의 프로젝트 생성 버튼 위치 소폭 수정

--- solchive/src/components/Header.js
+++ solchive/src/components/Header.js
@@ -4,12 +4,16 @@ import '../App.css'
 // TODO: a -> Link로 수정
 class Header extends Component {
     render() {
+        const btnMarginStyle = {
+            marginRight: "2rem"
+        }
+
         return (
             <Fragment>
                 <header className="header">
                     <a href="/projects" className="logo"><span>SOLCHIVE</span></a>
                     <div className="spacer"></div>
-                    <a href="/create"><button type="button" className="btn">프로젝트 추가</button></a>
+                    <a href="/create"><button type="button" className="btn" style={btnMarginStyle}>프로젝트 추가</button></a>
                 </header>
             </Fragment>
         );
