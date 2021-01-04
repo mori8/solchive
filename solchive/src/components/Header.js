857cf8004df00115f2c71322c097de6dc4454dd2
mori8 <kaithape@gmail.com>
2021-01-04 18:43:24 +0900

    로고 클릭하면 /projects로 리다이렉트되도록 수정

--- solchive/src/components/Header.js
+++ solchive/src/components/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
         return (
             <Fragment>
                 <header className="header">
-                    <a href="/board" className="logo"><span>SOLCHIVE</span></a>
+                    <a href="/projects" className="logo"><span>SOLCHIVE</span></a>
                     <div className="spacer"></div>
                     <a href="/create"><button type="button" className="btn">프로젝트 추가</button></a>
                 </header>
