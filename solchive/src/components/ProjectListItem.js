84bff4431cb25922983ae79c047ff11267a5397d
mori8 <kaithape@gmail.com>
2021-01-06 18:35:21 +0900

    ProjectItem 마우스오버 이벤트 디자인 추가

--- solchive/src/components/ProjectListItem.js
+++ solchive/src/components/ProjectListItem.js
@@ -2,30 +2,27 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 class ProjectListItem extends Component {
     render() {
-        const { id, title, team, period, body_text, body_images } = this.props.info;
+        const { id, title, team, period, summary, body_images } = this.props.info;
         return (
             <div className="project--item">
                 <Link to={`/projects/${id}`}>
-                    <div className="project--textarea">
-                        <div className="project--title--wrapper">
-                            <h4 className="project--title">{title}</h4>
-                        </div>
-                    </div>
-                    <div className="project--image--wrapper">
-                        <div className="project--image">
-                            <div className="centered">
-                                <img src="background.jpg"/>
+                    <div className="project--contents">
+                        <div className="project--textarea">
+                            <div className="project--title--wrapper">
+                                <h4 className="project--title">{title}</h4>
                             </div>
+                        </div>
+                        <div className="project--image--wrapper">
+                            <img src="background.jpg"/>
                         </div>
                     </div>
                     { /* TODO: 마우스 오버 시 이벤트. 위에껄 div로 묶고 hover -> display: none */ }
-                    <div className="project--details--hover">
+                    <div className="project--item--hover">
                         <h4 className="project--title">{title}</h4>
                         <div className="project--details">
                             <span className="project--team">{team}</span>
-                            <span className="project--period">{period}</span>
                         </div>
-                        <p className="project--description">{body_text}</p>
+                        <p className="project--description">{summary}</p>
                     </div>
                 </Link>
             </div>
