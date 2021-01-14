a3fcd9eb4221ba74a19086a9867f3271670e2689
mori8 <kaithape@gmail.com>
2021-01-15 02:59:13 +0900

    소감 추가 관련 이벤트 핸들러 구현
    - 각 input에 class 추가
    - CreateProject.js에서 인원 수에 따라 + 버튼을 클릭하여 input 창을 추가할 수 있도록 구현

--- solchive/src/components/CreateProject.js
+++ solchive/src/components/CreateProject.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { post } from 'axios';
+import './projects.css';
 
 class CreateProject extends Component {
     constructor(props) {
@@ -13,7 +14,18 @@ class CreateProject extends Component {
             body_images: null,
             file_name: "",
             summary: "",
-            git_url: ""
+            git_url: "",
+            name1: "",
+            comment1: "",
+            name2: "",
+            comment2: "",
+            name3: "",
+            comment3: "",
+            name4: "",
+            comment4: "",
+            name5: "",
+            comment5: "",
+            index: 1
         };
         this.handleFileChange = this.handleFileChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
@@ -39,6 +51,7 @@ class CreateProject extends Component {
     handleValueChange(e) {
         let nextState = {};
         nextState[e.target.name] = e.target.value;
+        console.log(nextState);
         this.setState(nextState);
     }
 
@@ -53,12 +66,38 @@ class CreateProject extends Component {
         formData.append('body_images', this.state.body_images);
         formData.append('summary', this.state.summary);
         formData.append('git_url', this.state.git_url);
+        formData.append('name1', this.state.name1);
+        formData.append('comment1', this.state.comment1);
+        formData.append('name2', this.state.name2);
+        formData.append('comment2', this.state.comment2);
+        formData.append('name3', this.state.name3);
+        formData.append('comment3', this.state.comment3);
+        formData.append('name4', this.state.name4);
+        formData.append('comment4', this.state.comment4);
+        formData.append('name5', this.state.name5);
+        formData.append('comment5', this.state.comment5);
 
         const config = {
             'content-type': 'multipart/form-data'
         }
 
         return post(url, formData, config);
+    }
+
+    addCommentsHandler = () => {
+        let commentsWrapper = document.querySelector(".create--comments--wrapper");
+        let comments = document.querySelector(".create--comments").cloneNode(true);
+        let nameInput = comments.childNodes[0].childNodes[1];
+        let impressionInput = comments.childNodes[1].childNodes[1];
+
+        nameInput.setAttribute("name", "name" + ++this.state.index);
+        nameInput.addEventListener("change", this.handleValueChange);
+        nameInput.value = "";
+        impressionInput.setAttribute("name", "comment" + this.state.index);
+        impressionInput.addEventListener("change", this.handleValueChange);
+        impressionInput.value = "";
+
+        commentsWrapper.appendChild(comments);
     }
 
     render() {
@@ -93,42 +132,56 @@ class CreateProject extends Component {
                 <h3>프로젝트 생성하기</h3>
                 <form style={formStyle} onSubmit={this.handleFormSubmit} method="post"> 
                     <div className="form-row">
-                        <div className="form-group col-md-6">
+                        <div className="form-group col-md-6 create--title">
                             <label>프로젝트 제목</label>
                             <input type="text" name="title" className="form-control" placeholder="제목" value={this.state.title} onChange={this.handleValueChange}/>
                         </div>
-                        <div className="form-group col-md-6">
+                        <div className="form-group col-md-6 create--framework">
                             <label>사용 프레임워크</label>
                             <input type="text" name="framework" value={this.state.framework} className="form-control" placeholder="쉼표(,)로 구분하여 입력해 주세요." onChange={this.handleValueChange}/>
                         </div>
                     </div>
                     <div className="form-row">
-                        <div className="form-group col-md-6">
+                        <div className="form-group col-md-6 create--team">
                             <label>팀명</label>
                             <input type="text" name="team" className="form-control" placeholder="팀명" value={this.state.team} onChange={this.handleValueChange}/>
                         </div>
-                        <div className="form-group col-md-6">
+                        <div className="form-group col-md-6 create--github">
                             <label>프로젝트 Github URL</label>
                             <input type="text" name="git_url" value={this.state.git_url} className="form-control" placeholder="Github URL" onChange={this.handleValueChange}/>
                         </div>
                     </div>
                     <div className="form-row">
-                        <div className="form-group col-md-6">
+                        <div className="form-group col-md-6 create--period">
                             <label>진행 기간</label>
                             <input type="text" name="period" className="form-control" placeholder="제목" value={this.state.period} onChange={this.handleValueChange}/>
                         </div>
-                        <div className="form-group col-md-6">
+                        <div className="form-group col-md-6 create--bodyimages">
                             <label>대표 이미지</label>
                             <input type="file" name="body_images" id="body_images" file={this.state.body_images} value={this.state.file_name} className="form-control" onChange={this.handleFileChange}/>
                         </div>
                     </div>
-                    <div className="form-group" style={textAreaStyle}>
+                    <div className="form-group create--summary" style={textAreaStyle}>
                         <label>프로젝트에 대해 한 문장으로 간략하게 설명해 주세요.</label>
                         <textarea className="form-control" name="summary" value={this.state.summary} rows="1" onChange={this.handleValueChange}></textarea>
                     </div>
-                    <div className="form-group" style={textAreaStyle}>
+                    <div className="form-group create--bodytext" style={textAreaStyle}>
                         <label>프로젝트에 대해 자세하게 설명해 주세요.</label>
                         <textarea className="form-control" name="body_text" value={this.state.body_text} rows="20" onChange={this.handleValueChange}></textarea>
+                    </div>
+                    <div className="form-group create--comments--wrapper" style={textAreaStyle}>
+                        <label>이 프로젝트에 참가한 팀원들의 소감을 들려주세요. 오른쪽의 + 버튼을 눌러 팀원을 추가할 수 있습니다. (최대 5명)</label>
+                        <button type="button" onClick={this.addCommentsHandler} className="btn create--comments--addbtn">+</button>
+                        <div className="create--comments" key="1">
+                            <div className="form-group create--bodytext">
+                                <label>이름</label>
+                                <input type="text" name="name1" className="form-control create--comment--name" placeholder="이름" onChange={this.handleValueChange}/>
+                            </div>
+                            <div className="form-group create--bodytext">
+                                <label>소감</label>
+                                <textarea className="form-control create--comment--impression" name="comment1"  onChange={this.handleValueChange}></textarea>
+                            </div>
+                        </div>
                     </div>
                     <div style={btnDivStyle}>
                         <button type="submit" className="btn btn-primary" style={btnStyle}>작성하기!</button>
