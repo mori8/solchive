596c01bfb9ac2eeef0e8d6a8a1140a00b1120192
mori8 <kaithape@gmail.com>
2021-01-30 23:44:34 +0900

    Impression component 완성
    - 팀원 수만큼만의 말풍선 생성
    - 각 말풍선마다 다른 색, 이모지 적용

--- solchive/src/components/Projects/Impression/Impression.js
+++ solchive/src/components/Projects/Impression/Impression.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import style from './Impression.module.css';
 
 class Impression extends Component {
     render() {
-        const { name, imoji, text, bg_color } = this.props;
+        const { name, imoji, text, bgColor } = this.props;
         return (
-            <div>
-                
+            <div className={style.wrapper}>
+                <div className={style.answer_wrapper}>
+                    <p className={style.contents} style={{backgroundColor: `${bgColor}`}}>{text}</p>
+                    <div className={style.profile}>
+                        <div className={style.tail_wrapper}>
+                            <div className={style.before_interviewee} style={{borderColor: `transparent transparent transparent ${bgColor}`}}></div>
+                            <p className={style.interviewee}>{imoji}</p>
+                        </div>
+                        <p className={style.name}>{name}</p>
+                    </div>
+                </div>
             </div>
         );
     }
