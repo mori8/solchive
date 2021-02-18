039bdf1da08c704d3701c6c7c12126ab598d0424
yena777 <parkyena01@sookmyung.ac.kr>
2021-02-18 18:23:13 +0900

    다중 이미지 업로드 및 업데이트

--- solchive/src/components/Projects/UpdateProject/UpdateProject.js
+++ solchive/src/components/Projects/UpdateProject/UpdateProject.js
@@ -121,7 +121,8 @@ class UpdateProject extends Component {
 
     handleFileChange(e) {
         this.setState({
-            body_images: e.target.files[0],
+            body_images: e.target.files,
+            file_name: e.target.value
         });
     }
 
@@ -131,7 +132,7 @@ class UpdateProject extends Component {
             console.log(res);
         });
         alert("수정 완료되었습니다.");
-        window.location.href = '/';
+        //window.location.href = '/';
     }
 
     handleValueChange(e) {
@@ -152,7 +153,9 @@ class UpdateProject extends Component {
         formData.append('period', this.state.period);
         formData.append('framework', this.state.framework);
         formData.append('body_text', this.state.body_text);
-        formData.append('body_images', this.state.body_images);
+        for(let i =0; i< this.state.body_images.length; i++){
+            formData.append("body_images", this.state.body_images[i]);
+        }
         formData.append('summary', this.state.summary);
         formData.append('git_url', this.state.git_url);
         formData.append('name1', this.state.name1);
@@ -212,14 +215,19 @@ class UpdateProject extends Component {
         let comments = document.querySelector("." + style.comments);
         let nameInput = comments.childNodes[0].childNodes[1];
         let impressionInput = comments.childNodes[1].childNodes[1];
+        
         this.setState((prevState) => {
             index: --prevState.index
         })
+        
+        nameInput.addEventListener("change", this.handleValueChange);
+        impressionInput.addEventListener("change", this.handleValueChange);
 
         nameInput.value = "";
         impressionInput.value = "";
         
         comments.remove();
+
 
 
     }
@@ -264,7 +272,7 @@ class UpdateProject extends Component {
                         </div>
                         <div className="form-group col-md-6">
                         <label>대표 이미지</label>
-                            <input type="file" name="body_images" id="body_images" file={this.state.body_images} className="form-control" onChange={this.handleFileChange} ref={this.fileInput}/>
+                            <input type="file" name="body_images" id="body_images" file={this.state.body_images} className="form-control"  multiple="multiple" onChange={this.handleFileChange} ref={this.fileInput}/>
                         </div>
                     </div>
                     <div className={"form-group " + style.form_textarea}>
@@ -282,7 +290,7 @@ class UpdateProject extends Component {
                             <div className="form-group update--bodytext">
                                 <label>이름</label>
                                 <input type="text" name="name1" className="form-control" value={this.state.name1} placeholder="이름" onChange={this.handleValueChange}/>
-                                {/*<button onClick={this.subtractCommentHandler}>삭제</button>*/}
+                                <button type="button" onClick={this.subtractCommentHandler}>삭제</button>
                             </div>
                             <div className="form-group update--bodytext">
                                 <label>소감</label>
